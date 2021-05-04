1949cba766424ac2ac326ee5cc9952131f56eeb0
jbped <pedigojacob@gmail.com>
2021-05-04 09:55:46 -0600

    Learning the ropes, added a couple variables for names and a fight function

--- assets/js/game.js
+++ assets/js/game.js
@@ -1,1 +1,19 @@
-window.alert('Hello World!')
+var playerName = window.prompt('What is your robo-gladiator\'s name?');
+
+console.log(playerName);
+
+console.log("Hello World!");
+
+console.log(10+10);
+
+console.log(playerName + "! Welcome to the Robo-Gladiator Brawl!")
+
+var name = "your name";
+console.log(name);
+
+// Creates a custom function that can be referred to later
+function fight () {
+    window.alert('The fight has BEGUN!');
+}
+
+fight ();
